2259e62fff9e685abfae5f0194793e0583570284
Sashen <SGovindasamy@dvtsoftware.com>
2026-03-02 01:02:42 +0200

    Saved changes to app including import

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import './App.css'
 
+import TemperatureConverter from './components/TemperatureConverter';
+
 const App = () => (
   <div>
     <h1>Temperature App</h1>
+    <TemperatureConverter />
   </div>
 );
 
