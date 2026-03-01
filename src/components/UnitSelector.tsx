7965aa1a3cc8ea1610d86990f80514d73f161414
Sashen <SGovindasamy@dvtsoftware.com>
2026-03-01 23:57:05 +0200

    Added controlled UnitSelector dropdown component

---
+++ src/components/UnitSelector.tsx
@@ -0,0 +1,20 @@
+import type { JSX } from "react";
+
+interface Props {
+    scale: 'c' | 'f';
+    onScaleChange: (scale: 'c' | 'f') => void;
+}
+
+const UnitSelector = ({ scale, onScaleChange}: Props): JSX.Element => {
+    return (
+        <select
+            value={scale}
+            onChange={(e) => onScaleChange(e.target.value as 'c' | 'f')}
+        >
+            <option value="c">Celsius</option>
+        <option value="f">Fahrenheit</option>
+    </select>
+    );
+};
+
+export default UnitSelector;
